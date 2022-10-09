321bcf8b26c37ea0d6c7a40ef66898d05cb7f714
pluffpenguin <roboperson10@gmail.com>
2022-10-09 16:25:10 -0700

    Edited getPerson()

--- notion.js
+++ notion.js
@@ -10,39 +10,77 @@ const notion = new Client({
     auth: process.env.NOTION_KEY,
 })
 
-class NotionDatabase {
-    
-    constructor (TABLE_DEADLINES_ID){
-        console.log("The NotionDatabase is being created!");
-        (async () => {
-            const response = await notion.databases.query({
-                database_id: TABLE_DEADLINES_ID
-            });
-        
-            //console.log(response.results[1]['properties']['Person']);
-        })();
-    }
-    
-    getTask = (name) => {
-       const thenoob = "hello9";
-    }
-    
 
-    getStatus = (name) => {
-        return {
+//console.log(response.results[1]['properties']['Person']);
+
+
+class NotionDatabase {
+
+    constructor (TABLE_DEADLINES_ID){
+        console.log("The NotionClient constructor has been run:");
+    }
+
+    getDatabase = async () => {
+        const response = await notion.databases.query({
+            database_id: TABLE_DEADLINES_ID
+        });
+        
+        // console.log(response)
+        console.log("response_results inside getDatabase(): ", response.results)
+        return response.results;
+    }
+
+    getTask = async (name) => {
+        const response = await notion.databases.query({
+            database_id: TABLE_DEADLINES_ID,
             filter: {
-                property: "Status",
+                property: "Task",
                 rich_text: {
                     contains: name
                 }
             }
+        });
+
+        return response;
+    }
+
+    getPerson = async (name) => {
+        // const response = await notion.databases.query({
+        //     database_id: TABLE_DEADLINES_ID,
+        //     filter: {
+        //         "properties": "Person",
+        //         "rich_text": {
+        //             "contains": name
+        //         }
+        //     }
+        // });
+        
+        const response = await notion.databases.query({
+            database_id: TABLE_DEADLINES_ID
+        });
+
+        for (let i = 0; i < response.results.length; i++){
+            console.log(response.results[i]['properties']['Person']['people'][0]['name']);
         }
+        // console.log(response.results[1]['properties']['Person']);
+        return response;
     }
 
 }
 
 database1 = new NotionDatabase(TABLE_DEADLINES_ID);
-console.log(database1.getTask("Jay"));
+// database1.getDatabase()
+
+database1.getPerson()
+
+// databaseResults = database1.getDatabase();
+
+// console.log("Reached checkpoint 2: ", databaseResults)
+// for (let i = 0; i < databaseResults.length; i++){
+//     console.log("Properties{}: \n", databaseResults['Properties'])
+// }
+
+
 /*
 (async () => {
     // How to retrieve a database? 
