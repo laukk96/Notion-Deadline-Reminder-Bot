b0cf8fcb7a0f478de6bfaa24b16866d52f33ec3a
Richard Azucenas <therichardazucenas@gmail.com>
2022-10-20 22:17:49 -0700

    updated notion functions for gdsc database

--- notion.js
+++ notion.js
@@ -4,7 +4,7 @@ const { getDatabase } = require('@notionhq/client/build/src/api-endpoints');
 
 // const databaseId = process.env.NOTION_DATABASE_ID;
 // How to share a database with an notion integration/connection? 
-const TABLE_DEADLINES_ID = "0f201482f6f1407899e8f7c8ae7dea28";
+const TABLE_DEADLINES_ID = "f944e134b0584cc289d0a97775384d76";
 //GDSC f944e134b0584cc289d0a97775384d76
 //NOTION DEV TEAM 0f201482f6f1407899e8f7c8ae7dea28
 
@@ -14,165 +14,121 @@ const notion = new Client({
 
 //just to check the objects in the properties
 const checkDataBase = async () => {
-    const payload = {
-        path: `databases/${TABLE_DEADLINES_ID}/query`,
-        method: 'POST'
-    }
-
-    const { results } = await notion.request(payload)
-
-    const detailData = results.map(page => {
-        console.log(page)
-    })
-}
-//locates a person's index
-getIndex = async (name) => {
-    const payload = {
-        path: `databases/${TABLE_DEADLINES_ID}/query`,
-        method: 'POST'
-    }
-    
-
-    const {results} = await notion.request(payload)
-
-    const people = results.map(page => {
-        return page.properties.Person.people[0].name
-    })
-
-    people.forEach((item, index, arr) => {
-        if (item.includes(name))
-        {
-            personIndex = index
-        }
+    const response = await notion.databases.query({
+        database_id: TABLE_DEADLINES_ID
     });
 
-    return personIndex
+    for (let i = 0; i < response.results.length; i++){
+        // console.log(response.results[i]['properties']['Person']['people'][0]['name']);
+        if (response.results[i]['properties']['Deadline']['date'] != null){
+            console.log("> Deadline Title: ", response.results[i]['properties']['Task']['title'][0]['plain_text']);
+            // console.log(response.results[i]['properties']['Taskee']['people']);
+            
+            // Print all the names of the people in a deadline
+            const peopleArray = response.results[i]['properties']['Taskee']['people'];
+            for (let j = 0; j < peopleArray.length; j++){
+                console.log('Officer Name: ', peopleArray[j]['name']);
+                //console.log('Email: ', peopleArray[j]['person']['email']);
+                console.log();
+            }
+
+            console.log('Finish Date: ', response.results[i]['properties']['Deadline']['date']['start']);    
+            console.log('\n======================================================');
+        }
+    }
 }
 
-
-class NotionDatabase {
+class NotionDatabase 
+{
 
     constructor (connectDatabase)
     {
-        this.connectDatabase = connectDatabase
+        this.connectDatabase = connectDatabase;
         console.log("The NotionDatabase is being created!");
         (async () => {
             const response = await notion.databases.query({
                 database_id: connectDatabase
             });
-        
-            //console.log(response.results[1]['properties']['Person']);
         })();
     }
     
-    //sendDeadlines() =>
+    //pushDeadlines() =>
 
-    getPerson = async (name) => {
-        let personName;
-        let personIndex;
-        
-        console.log("Getting " + name + "...")
-        
-        const payload = {
-            path: `databases/${TABLE_DEADLINES_ID}/query`,
-            method: 'POST'
-        }
-        
-        const {results} = await notion.request(payload)
-
-        
-        const peopleNameList = results.map(page => {
-            return page.properties.Person.people[0].name
-
-        })
-
-        const personID = results.map(page => {
-            return page.properties.Person.people[0].id
-        })
-        
-        peopleNameList.forEach((item, index, arr) => {
-            if (item.includes(name))
-            {
-                personName = item
-                personIndex = index
-            }
+    getPerson = async (deadline) => {
+        const response = await notion.databases.query({
+            database_id: TABLE_DEADLINES_ID
         });
 
+        for (let i = 0; i < response.results.length; i++){
+            // console.log(response.results[i]['properties']['Person']['people'][0]['name']);
+            if (response.results[i]['properties']['Deadline']['date'] != null){
+                if (response.results[i]['properties']['Task']['title'][0]['plain_text'].includes(deadline))
+                {
+                    console.log("> Deadline Title: ", response.results[i]['properties']['Task']['title'][0]['plain_text']);
+                    // console.log(response.results[i]['properties']['Taskee']['people']);
+                    
+                    // Print all the names of the people in a deadline
+                    const peopleArray = response.results[i]['properties']['Taskee']['people'];
+                    for (let j = 0; j < peopleArray.length; j++){
+                        console.log('Officer Name: ', peopleArray[j]['name']);
+                        console.log('Email: ', peopleArray[j]['person']['email']);
+                        console.log();
+                    }
 
-        return personName +", ID: " + personID[personIndex]
-        
-        /*
-        for (let i = 0; i < peopleList.length; i++)
-    {
-            if (peopleList[i].indexOf(name) == i)
-            {
-                personName = peopleList[i]
-                console.log(personName)
-            } else {
-                console.log("Invalid")
+                    console.log('Finish Date: ', response.results[i]['properties']['Deadline']['date']['start']);    
+                    console.log('\n======================================================');
+                }
             }
-    }
-    */
+        }
     }
 
     getTask = async (name) => {
-        console.log("Getting " + name + " task...")
+        console.log("Searching for " + name + "'s task...");
         
         const response = await notion.databases.query({
             database_id: TABLE_DEADLINES_ID
         });
     
-        const indexLoc = await getIndex(name);
-
-        return response.results[indexLoc]['properties']['Tasks']['rich_text'][0]['text']['content'];
-        
-    }
-
-    getStatus = async (name) => {
-        let personStatus;
-
-        const payload = {
-            path: `databases/${TABLE_DEADLINES_ID}/query`,
-            method: 'POST'
-        }
-        
-
-        const {results} = await notion.request(payload)
-
-        const indexLoc = await getIndex(name);
-
-        const status = results.map(page => {
-            return page.properties.Status.checkbox
-        })
-
-        status.forEach((item, index, arr) => {
-            if (index == indexLoc)
+        for (let i = 0; i < response.results.length; i++){
+            // console.log(response.results[i]['properties']['Person']['people'][0]['name']);
+            if (response.results[i]['properties']['Deadline']['date'] != null)
             {
-                personStatus = item
+                const peopleArray = response.results[i]['properties']['Taskee']['people'];
+                for (let j = 0; j < peopleArray.length; j++){
+                    if (peopleArray[j]['name'] != null)
+                    {
+                        if (peopleArray[j]['name'].includes(name))
+                        {
+                            console.log('\n======================================================');
+                            console.log();
+                            console.log(response.results[i]['properties']['Task']['title'][0]['plain_text']);
+                            console.log('Finish Date: ', response.results[i]['properties']['Deadline']['date']['start']);
+                        }
+                    }
+                }
             }
-        });
-
-        if (personStatus == true)
-        {
-            return name + " has this task completed."
-        } else {
-            return name + " has this task incomplete."
         }
-
+        console.log('\n======================================================');
     }
-
-    getDueDate = async (name) => {
     
-        console.log("Getting " + name + " due date...")
-        
+
+    getDueDate = async (deadline) => 
+    {
         const response = await notion.databases.query({
             database_id: TABLE_DEADLINES_ID
         });
     
-        const indexLoc = await getIndex(name);
-
-        return response.results[indexLoc]['properties']['Finish Date']['date']['end'];
-        
+        for (let i = 0; i < response.results.length; i++)
+        {
+            // console.log(response.results[i]['properties']['Person']['people'][0]['name']);
+            if (response.results[i]['properties']['Deadline']['date'] != null)
+            {
+                if (response.results[i]['properties']['Task']['title'][0]['plain_text'].includes(deadline))
+                {
+                    console.log('Finish Date: ', response.results[i]['properties']['Deadline']['date']['start']);
+                }
+            }
+        }
     }
 
 }
@@ -188,13 +144,23 @@ class NotionDatabase {
 
 database1 = new NotionDatabase(TABLE_DEADLINES_ID);
 
+/*
 (async() => {
     const response = await notion.databases.query({
         database_id: TABLE_DEADLINES_ID
     });
-
-    const tester = await database1.getTask("Kon");
-    console.log(tester);
+    for (let i = 0; i < response.results.length; i++)
+    {   
+        if (response.results[i]['properties']['Deadline']['date'] != null)
+        {
+        console.log(response.results[i]['properties']['Task']['title'][0]['plain_text']);
+        }
+    }
+    
 })();
+*/
+
+database1.getTask("Afraz");
 
 //checkDataBase();
+
