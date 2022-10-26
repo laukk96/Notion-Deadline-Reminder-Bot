6f7d0a73538f07b5b946201cf73d7d8179e6691d
KonstantinVVictoria <konstantinvictoria@gmail.com>
2022-10-25 20:40:37 -0700

    added ratelimiter scaffold

--- mongodb/collections.js
+++ mongodb/collections.js
@@ -1,5 +1,2 @@
 const { UserRegistry } = require("./collections/UserRegistry/UserRegistry");
-const {
-  DeadlineHistory,
-} = require("./collections/DeadlineHistory/DeadlineHistory");
-module.exports = { UserRegistry, DeadlineHistory };
+module.exports = { UserRegistry };
